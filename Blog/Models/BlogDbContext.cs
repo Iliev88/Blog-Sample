d95022435cc3efc262bca8bd1b1063f8fb5b9d90
Iliev88 <iliev.ilya@yahoo.com>
2017-04-07 17:45:25 +0300

    Creating Article entity and listing articles

--- Blog/Models/BlogDbContext.cs
+++ Blog/Models/BlogDbContext.cs
@@ -15,6 +15,8 @@ namespace Blog.Models
         {
         }
 
+        public virtual IDbSet<Article> Articles { get; set; }
+
         public static BlogDbContext Create()
         {
             return new BlogDbContext();
